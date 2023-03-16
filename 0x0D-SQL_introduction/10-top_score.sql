9d1b7170a2d91cf3391d2387ae79deda7ec74679
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-03-16 03:35:17 -0700

    Task 10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 #!/bin/bash
 -- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 SELECT 'score', 'name'
-FROM second_table
-ORDER BY 'score', 'name' ASC|DESC;
+FROM 'second_table'
+ORDER BY 'score' DESC;
