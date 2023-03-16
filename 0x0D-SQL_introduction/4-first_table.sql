f68c528dc9738ca0300229a947d5bdebb6432188
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-03-16 02:06:33 -0700

    Task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,6 @@
 #!/bin/bash
 -- creates a table called first_table in the current database in your MySQL server
-CREATE IF NOT EXISTS first_table ( id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
