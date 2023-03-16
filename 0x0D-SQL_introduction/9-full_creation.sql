fcc81b2023298f487c2d0fcbc72fcdf51e528673
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-03-16 03:20:15 -0700

    Task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,4 @@
 #!/bin/bash
 -- creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows
-CREATE TABLE IF NOT EXISTS second_table(
-	id INT,
-	name VARCHAR(256),
-	score INT);
-INSERT INTO table_name(id, name, score) VALUES(1, "John", 10);
-INSERT INTO table_name(id, name, score) VALUES(2, "Alex", 3);
-INSERT INTO table_name(id, name, score) VALUES(3, "Bob", 14);
-INSERT INTO table_name(id, name, score) VALUES(4, "George", 8);
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
