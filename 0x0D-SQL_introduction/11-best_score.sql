ba33653a69939f7d82d1dcf58bd818109264a28c
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-03-16 03:50:48 -0700

    Task 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,3 @@
 #!/bin/bash
 -- lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE hbtn_0c_0;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
