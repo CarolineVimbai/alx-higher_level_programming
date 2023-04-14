c5c516a9487a90623fcacbef74c59ec46452b6c3
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-04-14 12:08:53 -0700

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const numArgs = process.argv.length - 2;
-if (numArgs == 0)
+if (numArgs === 0)
 {
 	console.log('No argument');}
-else if (numArgs == 1)
+else if (numArgs === 1)
 {
 	console.log('Argument found');}
 else
 {
-	console.log('${numArgs} Arguments found');}
+	console.log('Arguments found');}
