95672025e4a4398826838866da44a39b356514b3
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-04-18 06:47:13 -0700

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const [myarg] = process.argv.slice(2);
-if (myarg) {
-  console.log(arg);
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-  console.log("No argument");
+  console.log(process.argv[2]);
 }
-
