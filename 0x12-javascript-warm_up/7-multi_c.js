4781fb153258f9f6849515c65049525600fb351e
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-04-18 06:04:01 -0700

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const Xtimes = parseInt(process.argv[2]);
-if (intconv(Xtimes)) {
+if (isNaN(Xtimes)) {
   console.log('Missing number of occurrences');
 } else {
   for (let x = 0; x < Xtimes; x++) {
