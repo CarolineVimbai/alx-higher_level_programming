a49bd53775b4fbdc62a4d1fd94d808e33183d259
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-04-18 06:52:38 -0700

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const first_int = parseInt(process.argv[2]);
-const second_int = parseInt(process.argv[3]);
+const firstint = parseInt(process.argv[2]);
+const secondint = parseInt(process.argv[3]);
 function add (x, y) {
   if (isNaN(x) || isNaN(y)) {
     return NaN;
   }
   return x + y;
 }
-console.log(add(first_int, second_int));
+console.log(add(firstint, secondint));
