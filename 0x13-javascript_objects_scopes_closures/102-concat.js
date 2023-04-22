72bcddad991cc1a0c769eba52830b73f2a53ff11
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-04-22 10:06:03 -0700

    Task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,27 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('process');
 const fs = require('fs');
 
-if (argv.length === 5) {
-  fs.readFile(argv[2], 'utf8', function (err, data) {
-    if (err) {
-      console.log(err);
-    }
-    fs.writeFile(argv[4], data, { encoding: 'utf8', flag: 'a+', mode: 0o666 }, (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  });
-
-  fs.readFile(argv[3], 'utf8', function (err, data) {
-    if (err) {
-      console.log(err);
-    }
-    fs.writeFile(argv[4], data, { encoding: 'utf8', flag: 'a+', mode: 0o666 }, (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  });
-}
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
