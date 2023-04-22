d79c98bb264ac52ce4282722551a512f0be6eec4
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-04-22 10:34:18 -0700

    Task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
+
 const dict = require('./101-data').dict;
-const newKey = [];
 const newDict = {};
-let i = 0;
-for (const [key, value] of Object.entries(dict)) {
-  if (!newKey.includes(value)) {
-    newKey.push(value);
-    newDict[newKey[i]] = [key];
-    i++;
-  } else {
-    for (let j = 0; newKey[j]; j++) {
-      if (newKey[j] === value) {
-        newDict[newKey[j]].push(key);
-      }
-    }
+
+Object.keys(dict).map(function (key) {
+  if (!Array.isArray(newDict[dict[key]])) {
+    newDict[dict[key]] = [];
   }
-}
+  newDict[dict[key]].push(key);
+});
+
 console.log(newDict);
