9e87362347eac0a867eea999cd85df940b90c8d2
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-04-30 16:44:12 -0700

    Task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    request = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    print('{}'.format(request.json().get('id')))
