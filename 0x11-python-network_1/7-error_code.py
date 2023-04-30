f88be75b456114ae9fd3d8b13a5a023f26c18b35
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-04-30 16:34:23 -0700

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" sends a request to the URL and displays the body of the response"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    request = requests.get(sys.argv[1])
+    if request.status_code <= 400:
+        print(request.text)
+    else:
+        print("Error code: {}".format(request.status_code))
