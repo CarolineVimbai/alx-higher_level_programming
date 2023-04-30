ca48d36144f61fd0750ad5bb5caa402f30b3d8b7
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-04-30 16:19:01 -0700

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)"""
+
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({"email": sys.argv[2]}).encode("ascii")
+    with urllib.request.urlopen(sys.argv[1], data) as response:
+        html = response.read()
+        print(html.decode("UTF-8"))
