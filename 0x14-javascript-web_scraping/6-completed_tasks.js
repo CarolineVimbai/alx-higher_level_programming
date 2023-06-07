ea8f7bfd22923122b5c92e108c11db8343d22965
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-06-07 05:40:06 -0700

    Task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request(url, { json: true }, (err, res, body) => {
-  if (err) { console.log(err); } else {
-    const result = {};
-    for (const data of body) {
-      if (data.completed) {
-        if (result[data.userId]) {
-          result[data.userId] += 1;
-        } else {
-          result[data.userId] = 1;
-        }
+
+request(url, (err, resp, body) => {
+  if (err) { console.log(err); }
+
+  const completed = {};
+  const jsonBody = JSON.parse(body);
+  for (const task of jsonBody) {
+    if (task.completed) {
+      if (completed[task.userId]) {
+        completed[task.userId]++;
+      } else {
+        completed[task.userId] = 1;
       }
     }
-    console.log(result);
   }
+  console.log(completed);
 });
